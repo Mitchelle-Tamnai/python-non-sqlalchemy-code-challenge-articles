7852fcdfdc904be42f45ba1579e20d2ca8d22526
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-06-02 22:48:33 +0300

    Commit changes

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,8 +1,52 @@
 class Article:
+    all = []
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        self.all.append(self)
+        if not isinstance(author, Author):
+            raise Exception("Author must be of type Author")
+        if not isinstance(magazine, Magazine):
+            raise Exception("Magazine must be of type Magazine")
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise Exception("Title must be a string with 5 to 50 characters")
+
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+
+        author.articles().append(self)
+        magazine.articles().append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, new_magazine):
+        if not isinstance(new_magazine, Magazine):
+            raise Exception("Magazine must be of type Magazine")
+        self._magazine.articles().remove(self)
+        new_magazine.articles().append(self)
+        self._magazine = new_magazine
+
+    @author.setter
+    def author(self, new_author):
+        if not isinstance(new_author, Author):
+            raise Exception("Author must be of type Author")
+        self._author.articles().remove(self)
+        new_author.articles().append(self)
+        self._author = new_author
+
         
 class Author:
     def __init__(self, name):
